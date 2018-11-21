71b4d61c01609b52dfcddc2aa57847d843c8262f
David Canagasabey <david.canagasabey@mail.utoronto.ca>
2018-11-21 18:02:29 -0500

    Added buttons to select Minesweeper and Grid Filler

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/SelectingGameActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/SelectingGameActivity.java
@@ -3,6 +3,7 @@ package fall2018.csc2017.slidingtiles;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * The activity in choosing which game to play.
@@ -19,6 +20,8 @@ public class SelectingGameActivity extends GameAppCompatActivity {
         currentCentre = GameCentre.getInstance(this);
         setContentView(R.layout.activity_selecting_game);
         addTilesButtonListener();
+        addMineButtonListener();
+        addGridButtonListener();
     }
 
     /**
@@ -34,4 +37,33 @@ public class SelectingGameActivity extends GameAppCompatActivity {
             }
         });
     }
+
+    /**
+     * Choose the minesweeper game.
+     */
+    private void addMineButtonListener() {
+        Button TileButton = findViewById(R.id.Minesweeper);
+        TileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCentre.setCurrentGame("MS");
+                Toast.makeText(SelectingGameActivity.this, "TODO: Begin playing Minesweeper", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Choose the grid filler game.
+     */
+    private void addGridButtonListener() {
+        Button TileButton = findViewById(R.id.GridFiller);
+        TileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCentre.setCurrentGame("GF");
+                Toast.makeText(SelectingGameActivity.this, "TODO: Begin playing Grid Filler", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
