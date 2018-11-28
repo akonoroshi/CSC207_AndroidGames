e1684331b30d05312a6eff7df2d0f63ad8fc05f6
Zhiheng Zou <zhiheng.zou@mail.utoronto.ca>
2018-11-28 17:36:01 -0500

    Added javadoc and more unit tests

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/gamecentre/MSTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/gamecentre/MSTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.TimerTask;
 
 import static org.junit.Assert.*;
 
@@ -15,8 +16,8 @@ public class MSTest {
 
     /**
      * Create an empty MSBoard with size
-     * @param size
-     * @return
+     * @param size the size of the board to be created
+     * @return a new MSBoard
      */
     private MSBoard createEmptyBoard(int size) {
         List<MSTile> tiles = new ArrayList<>();
@@ -42,6 +43,9 @@ public class MSTest {
         assertTrue(manager.puzzleSolved());
     }
 
+    /**
+     * Check whether puzzleSolved is working on a completely revealed board
+     */
     @Test
     public void testRevealedAndSolvedBoard() {
         MSBoard board = createEmptyBoard(5);
@@ -50,7 +54,10 @@ public class MSTest {
         board.revealAll();
         assertTrue(manager.puzzleSolved());
     }
-    
+
+    /**
+     * Check whether a tap is valid on a revealed board
+     */
     @Test
     public void testRevealedInvalidTap() {
         MSBoard board = createEmptyBoard(5);
@@ -62,6 +69,9 @@ public class MSTest {
         }
     }
 
+    /**
+     * Check whether the mines on a board are calculated correctly
+     */
     @Test
     public void testMinePopulation() {
             MSBoard board = createEmptyBoard(1);
@@ -75,6 +85,9 @@ public class MSTest {
             assertEquals(mines, board.getTotalMines());
     }
 
+    /**
+     * Check whether the tap is valid on a random position of the board
+     */
     @Test
     public void testInvalidTap() {
         Random random = new Random();
@@ -89,6 +102,9 @@ public class MSTest {
 
     }
 
+    /**
+     * Check whether the game is over on a MSBoard not received any action yet
+     */
     @Test
     public void testgameOverCheck1(){
         MSBoard board = createEmptyBoard(2);
@@ -96,6 +112,9 @@ public class MSTest {
         assertFalse(manager.gameOverCheck());
     }
 
+    /**
+     * Check whether the game is over on a solved MSBoard
+     */
     @Test
     public void testgameOverCheck2(){
         MSBoard board = createEmptyBoard(1);
@@ -104,6 +123,9 @@ public class MSTest {
         assertTrue(manager.gameOverCheck());
     }
 
+    /**
+     * Check whether the puzzle is solved on a solved board
+     */
     @Test
     public void testpuzzleSolved1(){
         MSBoard board = createEmptyBoard(1);
@@ -113,6 +135,9 @@ public class MSTest {
         assertTrue(manager.puzzleSolved());
     }
 
+    /**
+     * Check whether the puzzle is solved on a unsolved board
+     */
     @Test
     public void testpuzzleSolved2(){
         MSBoard board = createEmptyBoard(2);
@@ -124,6 +149,9 @@ public class MSTest {
         assertFalse(manager.puzzleSolved());
     }
 
+    /**
+     * Check whether the flag has an effect on the number of mines on the board
+     */
     @Test
     public void testFlag1(){
         MSBoard board = createEmptyBoard(1);
@@ -134,6 +162,9 @@ public class MSTest {
         assertEquals(0, manager.remainingMines);
     }
 
+    /**
+     * Check whether the flag has an effect on a tile of a board with a mine
+     */
     @Test
     public void testFlag(){
         MSBoard board = createEmptyBoard(1);
@@ -145,6 +176,10 @@ public class MSTest {
         assertEquals(1, manager.remainingMines);
     }
 
+    /**
+     * Check whether a press is valid on a blank tile
+     */
+
     @Test
     public void testisValidPress(){
         MSBoard board = createEmptyBoard(1);
@@ -152,6 +187,9 @@ public class MSTest {
         assertTrue(manager.isValidPress(0));
     }
 
+    /**
+     * Check whether the press is valid on blank tile with a flag
+     */
     @Test
     public void testisValidPress1(){
         MSBoard board = createEmptyBoard(1);
@@ -160,6 +198,9 @@ public class MSTest {
         assertTrue(manager.isValidPress(0));
     }
 
+    /**
+     * Check whether a press is valid on a revealed tile
+     */
     @Test
     public void testisValidPress2(){
         MSBoard board = createEmptyBoard(1);
@@ -168,6 +209,9 @@ public class MSTest {
         assertFalse(manager.isValidPress(0));
     }
 
+    /**
+     * Check whether the touchMove is valid on a blank tile
+     */
     @Test
     public void testtouchMove(){
         MSBoard board = createEmptyBoard(1);
@@ -176,6 +220,9 @@ public class MSTest {
         assertTrue(((MSTile)board.getTile(0)).isRevealed());
     }
 
+    /**
+     * Check whether adddMine could add a mine on a board with size 1
+     */
     @Test
     public void testaddMine(){
         MSBoard board = createEmptyBoard(1);
@@ -184,6 +231,9 @@ public class MSTest {
         assertEquals(1, manager.remainingMines);
     }
 
+    /**
+     * Check whether subtractMine on a board with size 1
+     */
     @Test
     public void testsubtractMine(){
         MSBoard board = createEmptyBoard(1);
@@ -193,6 +243,9 @@ public class MSTest {
         assertEquals(0, manager.remainingMines);
     }
 
+    /**
+     * Check whether a timer is working on a board with size 1
+     */
     @Test
     public void testTimer(){
         MSBoard board = createEmptyBoard(1);
@@ -201,13 +254,82 @@ public class MSTest {
         assertEquals(0, manager.timer);
     }
 
+    /**
+     * Check whether the tileDrawable return the correct file name for a tile
+     */
     @Test
     public void testTileDrawable(){
         MSBoard board = createEmptyBoard(1);
         MSManager manager = new MSManager(board);
-        assertEquals("ms_default", manager.getTileDrawable(0));
+        boolean result1 = manager.getTileDrawable(0).equals("ms_default");
+        ((MSTile)board.getTile(0)).setFlagged();
+        boolean result2 = manager.getTileDrawable(0).equals("ms_flagged");
+        assertTrue(result1 && result2);
     }
 
+    /**
+     * Check whether the tileDrawable return the correct file name for a tile
+     */
+    @Test
+    public void testTileDrawable2(){
+        MSBoard board = createEmptyBoard(1);
+        MSManager manager = new MSManager(board);
+        ((MSTile)board.getTile(0)).setMine();
+        ((MSTile)board.getTile(0)).setRevealed();
+        boolean result1 = manager.getTileDrawable(0).equals("ms_bomb");
+        assertTrue(result1);
+    }
+
+    /**
+     * Check whether the tileDrawable return the correct file name for a tile
+     */
+    @Test
+    public void testTileDrawable3(){
+        MSBoard board = createEmptyBoard(1);
+        MSManager manager = new MSManager(board);
+        ((MSTile)board.getTile(0)).setRevealed();
+        boolean result1 = manager.getTileDrawable(0).equals("ms_blank");
+        assertTrue(result1);
+    }
+
+    /**
+     * Check whether the tileDrawable return the correct file name for a tile
+     */
+    @Test
+    public void testTileDrawable4(){
+        MSBoard board = createEmptyBoard(4);
+        MSManager manager = new MSManager(board);
+        ((MSTile)board.getTile(0)).setRevealed();
+        boolean result1 = manager.getTileDrawable(0).equals("ms_blank");
+        assertTrue(result1);
+    }
+
+    /**
+     * Check whether the tileDrawable return the correct file name for a tile
+     */
+    @Test
+    public void testTileDrawable5(){
+        List<MSTile> tile = new ArrayList<MSTile>(0);
+        MSTile tile1 = new MSTile(0);
+        MSTile tile2 = new MSTile(1);
+        MSTile tile3 = new MSTile(2);
+        MSTile tile4 = new MSTile(3);
+        tile.add(tile1);
+        tile.add(tile2);
+        tile.add(tile3);
+        tile.add(tile4);
+        MSBoard board = new MSBoard(tile, 2, 2);
+        MSManager manager = new MSManager(board);
+        ((MSTile)board.getTile(1)).setMine();
+        board.mineLocations = new ArrayList<>(0);
+        board.mineLocations.add(1);
+        ((MSTile)board.getTile(0)).setRevealed();
+        boolean result1 = manager.getTileDrawable(0).equals("ms_1");
+        assertTrue(result1);
+    }
+    /**
+     * Check whether the constructor of MSManager with 2 arguments work
+     */
     @Test
     public void testManager(){
         MSManager ms = new MSManager(1, 2);
@@ -215,6 +337,9 @@ public class MSTest {
         assertEquals(0, ms.remainingMines);
     }
 
+    /**
+     * Check whether the constructor of MSManager that takes no argumants work
+     */
     @Test
     public void testManager1(){
         MSManager ms = new MSManager();
@@ -222,14 +347,22 @@ public class MSTest {
         assertEquals(0, ms.remainingMines);
     }
 
+    /**
+     * Check whether the getter for MSBoard and getScore are working
+     */
     @Test
     public void testBoardandScore(){
         MSManager ms = new MSManager();
         int i = ms.getScore();
+        boolean result1 = i == 0;
         Board b = ms.getBoard();
-        assertEquals(0, ms.remainingMines);
+        boolean result2 = b == ms.getBoard();
+        assertTrue(result1 && result2);
     }
 
+    /**
+     * Check whether the tap a valid on a board with size 1
+     */
     @Test
     public void testisValidTap(){
         MSBoard board = createEmptyBoard(1);
@@ -237,4 +370,15 @@ public class MSTest {
         assertTrue(manager.isValidTap(0));
     }
 
+    /**
+     * Check whether reveal is working on a board with a bombed tile
+     */
+    @Test
+    public void testReveal(){
+        MSBoard board = createEmptyBoard(1);
+        MSManager manager = new MSManager(board);
+        ((MSTile)board.getTile(0)).setMine();
+        board.reveal(0);
+        assertTrue(manager.gameOverCheck());
+    }
 }
