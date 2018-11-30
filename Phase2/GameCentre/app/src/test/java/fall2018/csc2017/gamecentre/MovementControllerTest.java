5492841893adab7c3960481383b0698e6803a013
Yuya <yuya.asano@mail.utoronto.ca>
2018-11-30 09:12:18 -0500

    Divided tests

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/gamecentre/MovementControllerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/gamecentre/MovementControllerTest.java
@@ -20,10 +20,23 @@ import fall2018.csc2017.gamecentre.slidingtiles.STManager;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Test if methods in MovermentController work properly
+ */
 public class MovementControllerTest {
+    /**
+     * Context needed to test MovermentController
+     */
     private Context con;
+
+    /**
+     * Instance of MovermentController
+     */
     private MovementController mc;
 
+    /**
+     * Class to get Context
+     */
     private static class TempActivity extends GameAppCompatActivity {
         private static Context context;
         @Override
@@ -32,32 +45,38 @@ public class MovementControllerTest {
             context = getContext();
         }
 
+        /**
+         * Help MovementControllerTest get Context
+         *
+         * @return Context of TempActivity
+         */
         static Context getContext() {
             return TempActivity.context;
         }
 
     }
 
-    private List<Tile> makeTiles(int boardSize) {
+    /**
+     * Make a solved STBoard and set it to STManager.
+     *
+     * @return STManager with a solved Board
+     */
+    private STManager setUpCorrect() {
+        int boardSize = 4;
         List<Tile> tiles = new ArrayList<>();
-        final int numTiles = boardSize * boardSize;
-        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+        for (int tileNum = 0; tileNum < boardSize * boardSize; tileNum++) {
             tiles.add(new Tile(tileNum));
         }
-
-        return tiles;
-    }
-
-    /**
-     * Make a solved Board.
-     */
-    private BoardManager setUpCorrect() {
-        int boardSize = 4;
-        List<Tile> tiles = makeTiles(boardSize);
         STBoard board = new STBoard(tiles, boardSize);
         return new STManager(board);
     }
 
+    /**
+     * Make a MSBoard with no mine and set it to MSManager.
+     *
+     * @param size size of MSBoard
+     * @return MSManager with MSBoard with no mine
+     */
     private MSManager createEmptyBoard(int size) {
         List<MSTile> tiles = new ArrayList<>();
         for (int i = 0; i < size * size; i++) {
@@ -68,47 +87,76 @@ public class MovementControllerTest {
         return new MSManager(board);
     }
 
+    /**
+     * Test processTapMovement for grid filler
+     */
     @Test
-    public void testProcessTapMovement() {
-        BoardManager manager = setUpCorrect();
-        mc.setBoardManager(manager);
-        mc.processTapMovement(con, 0, false);
-        assertTrue(manager.puzzleSolved());
-        mc.processTapMovement(con, 11, false);
-        assertFalse(manager.puzzleSolved());
-
-        manager = new GFManager(10, 3);
+    public void testProcessTapMovementGF() {
+        GFManager manager = new GFManager(10, 3);
         mc.setBoardManager(manager);
         mc.processTapMovement(con, 43, false);
-        assertTrue(((GFTile) manager.getBoard().getTile(43)).isPlaced());
+        assertTrue(manager.getBoard().getTile(43).isPlaced());
+    }
 
-        manager = createEmptyBoard(6);
+    /**
+     * Test processTapMovement for minesweeper
+     */
+    @Test
+    public void testProcessTapMovementMS() {
+        MSManager manager = createEmptyBoard(6);
         mc.setBoardManager(manager);
         mc.processTapMovement(con, 5, false);
         assertTrue(((MSTile) manager.getBoard().getTile(5)).isRevealed());
 
     }
 
+    /**
+     * Test processTapMovement for sliding tiles
+     */
     @Test
-    public void testProcessPressMovement() {
-        BoardManager manager = setUpCorrect();
+    public void testProcessTapMovementST() {
+        STManager manager = setUpCorrect();
         mc.setBoardManager(manager);
-        mc.processPressMovement(con, 3, false);
-        assertTrue(manager.puzzleSolved());
-        mc.processPressMovement(con, 14, false);
+        mc.processTapMovement(con, 11, false);
         assertFalse(manager.puzzleSolved());
+    }
 
-        manager = new GFManager(10, 3);
+    /**
+     * Test processPressMovement for grid filler
+     */
+    @Test
+    public void testProcessPressMovementGF() {
+        GFManager manager = new GFManager(10, 3);
         mc.setBoardManager(manager);
         mc.processPressMovement(con, 22, false);
-        assertTrue(((GFTile) manager.getBoard().getTile(22)).isPlaced());
+        assertTrue(manager.getBoard().getTile(22).isPlaced());
+    }
 
+    /**
+     * Test processPressMovement for minesweeper
+     */
+    @Test
+    public void testProcessPressMovementMS() {
         MSManager msManager = createEmptyBoard(6);
         mc.setBoardManager(msManager);
         mc.processPressMovement(con, 0, false);
         assertTrue(((MSTile) msManager.getBoard().getTile(0)).isFlagged());
     }
 
+    /**
+     * Test processPressMovement for sliding tiles
+     */
+    @Test
+    public void testProcessPressMovementST() {
+        STManager manager = setUpCorrect();
+        mc.setBoardManager(manager);
+        mc.processPressMovement(con, 14, false);
+        assertFalse(manager.puzzleSolved());
+    }
+
+    /**
+     * Initialize MovementController and Context
+     */
     @Before
     public void setUp() {
         mc = new MovementController();
